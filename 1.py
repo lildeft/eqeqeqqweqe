7524d35023138cdbcfe8d05909c2608552722fda
lildeft <eduba123456@gmail.com>
2022-09-14 21:59:32 -0300

    eeeeeeeeee prova

---
+++ 1.py
@@ -0,0 +1,10 @@
+valor1 = int(input('digite um numero: '))
+valor2 = int(input('digite um numero: '))
+valor3 = int(input('digite um numero: '))
+if valor1 > valor2 and valor3:
+    print('valor1')
+elif valor2 > valor3 and valor1:
+    print('valor2')
+else:
+    print('valor3')
+
---
+++ prova2.py
@@ -0,0 +1,11 @@
+idade = 40
+tempoDeServico = 20
+print(idade)
+print(tempoDeServico)
+if idade >= 65 or tempoDeServico >=30:
+    print('aposentadoria')
+elif idade >=60 and tempoDeServico >=25:
+    print('aposentadoria')
+else:
+    print('nao aposenta')
+
